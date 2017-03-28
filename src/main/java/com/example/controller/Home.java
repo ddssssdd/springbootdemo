77a41f6f0c447902506975296947f0349e4877a0
Steven Fu <fuxinyu@gmail.com>
2017-03-28 14:34:32 +0800

    add email sender

--- src/main/java/com/example/controller/Home.java
+++ src/main/java/com/example/controller/Home.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class Home {
+    @Autowired
+    private JavaMailSender mailSender;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("178771232@qq.com");
+        message.setTo("a060116@163.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
+
         return "Hello, this is a test";
     }
 
